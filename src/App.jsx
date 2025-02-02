c09f958114c1934ae1dcecd795d1b69c1dfaeb87
Julia Baur <juliabaur1@gmx.de>
2025-02-02 13:08:38 +0100

    State for to do list imported from react

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
+import React, { useState } from 'react';
 import TodoList from './components/TodoList';
 
 function App() {
+    const [todos, setTodos] = useState([
+    'First To-Do',
+    'Second To-Do',
+    'Third To-Do',
+    ]);
+
     return (
     <div>
         <h1>My To-Do List</h1>
-        <TodoList />
+        <TodoList todos={todos} />
     </div>
     );
 }
